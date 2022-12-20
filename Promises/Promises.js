1a6320b657a3685b26a938cc47a202d4be466a84
Faisal I <mfaisal.ilyas.d@gmail.com>
2022-12-20 11:51:35 +0530

    making promises

--- Promises/Promises.js
+++ Promises/Promises.js
@@ -1,13 +1,21 @@
-const FetchPromise = ()=>{
-    return fetch('data.json')
+// const FetchPromise = ()=>{
+//     return fetch('data.json')
+// }
+// const promise = FetchPromise()
+// .then(()=>{
+//     console.log("it worked")
+// })
+// .catch(()=>{
+//     console.log("Error")
+// })
+// .then(()=>{
+//     console.log("Line after the first catch ")
+// })
+const ReturnPromies = ()=>{
+    return new Promise ((resolve, reject)=>{
+        resolve({"check":"working", "not_check": "not working"})
+    })
 }
-const promise = FetchPromise()
-.then(()=>{
-    console.log("it worked")
-})
-.catch(()=>{
-    console.log("Error")
-})
-.then(()=>{
-    console.log("Line after the first catch ")
+ReturnPromies().then((data)=>{
+    console.log(data.not_check)
 })
