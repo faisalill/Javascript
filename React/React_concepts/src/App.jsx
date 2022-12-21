37cd0501e39de72602811d64bef0ea963cd667b3
Faisal I <mfaisal.ilyas.d@gmail.com>
2022-12-21 22:42:26 +0530

    useState and useEffects Done

--- React/React_concepts/src/App.jsx
+++ React/React_concepts/src/App.jsx
@@ -1,12 +1,13 @@
+import USEEFFECT from "./Hooks/USEEFFECT"
 import USEREF from "./Hooks/USEREF"
 import USESTATE from "./Hooks/USESTATE"
 
 
-
-function App() {
+function App() { 
   return (
     <>
-    <USESTATE />
+    {/* <USESTATE />
+    <USEEFFECT /> */}
     <USEREF />
     </>
   )
--- React/React_concepts/src/Hooks/USESTATE.jsx
+++ React/React_concepts/src/Hooks/USESTATE.jsx
@@ -22,6 +22,36 @@ const USESTATE = () => {
     {/* So we use the one defined above so when the 
     state of the counter changes it does not trigger the 
     rerender of the RandomLog state  */}
+    {/* And when we pass a function to useState it sets the 
+    current state to the return value of the function. 
+    The setState((value)=>return ...) here setSTate provides
+    the value of previous state as a parameter automatically and 
+    using this functional method to update state is better since 
+    it does not cause unusual rerender and avoids the problem occuring 
+    in the below provided example */}
+    <button onClick={
+        ()=>{
+            setCounter(Counter+1)
+            setTimeout(()=>{setCounter(Counter + 1)},300)
+            setTimeout(()=>{setCounter(Counter + 1)},300)
+            setTimeout(()=>{setCounter(Counter + 1)},300)
+        }
+    }>Usual update</button>
+    {/* on clicking the above button the counter remains
+    the same since the same value of counter is passed 
+    lets try it with functional state update */}
+    <button onClick={
+        ()=>{
+            setCounter((counter)=>counter+1)
+            setTimeout(()=>{setCounter((counter)=>counter+1)},300)
+            setTimeout(()=>{setCounter((counter)=>counter+1)},300)
+            setTimeout(()=>{setCounter((counter)=>counter+1)},300)
+        }
+    }>Functional update</button>
+    {/* Using this functional update the value updates 4 four
+    times like it should because instead of passing the same 
+    flushed out state value it passes the current state
+    through the function parametere and changes the value */}
     </div>
     </>
   )
