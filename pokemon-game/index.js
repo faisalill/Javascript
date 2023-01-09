02d6cae12be9331c559e92272fb3dac2b1ac53e0
faisalill <mfaisal.ilyas.d@gmail.com>
2023-01-09 23:18:13 +0530

    altar animation done

--- pokemon-game/index.js
+++ pokemon-game/index.js
@@ -107,13 +107,89 @@ const player = new Sprite({
     }
 })
 
+const waterAnimations = new Image()
+waterAnimations.src = './assets/water_animations.png'
 
+class animationSprite {
+    constructor({image, rows,columns, position, sizeOffset}){
+        this.image = image
+        this.rows = rows
+        this.columns = columns
+        this.frames = 0
+        this.framesElapsed = 0
+        this.position = position
+        this.sizeOffset = sizeOffset
+    }
+    draw(){
+        c.drawImage(this.image,
+           this.frames*(this.image.width/this.columns),0,this.image.width/this.columns,this.image.height/this.rows,
+           this.position.x,this.position.y,this.image.width/this.columns + this.sizeOffset,this.image.height/this.rows + this.sizeOffset
+            )
+            if(this.rows >1 || this.columns >1){
+               this.framesElapsed++
+            }
+            if(this.framesElapsed %10 === 0){
+                if(this.frames < this.columns -1){
+                    this.frames++
+                }
+                else{
+                    this.frames = 0
+                }
+            }
+        }
 
+        // if(this.frames.max >1){
+        //     this.frames.elapsed++
+        // }
+        // if(this.frames.elapsed % 10 ===0){
+        //     if(this.frames.val < this.frames.max -1) this.frames.val++
+        //     else this.frames.val = 0
+        // }
+
+        // c.drawImage(this.image,
+        //     this.frames.val * this.width,
+        //     0,
+        //     this.image.width/this.frames.max, 
+        //     this.image.height,
+        //     this.position.x,
+        //     this.position.y,
+        //     this.image.width/this.frames.max - this.sizeOffset ,
+        //     this.image.height - this.sizeOffset)
+        // }
+            
+            
+    
+}
+
+const testWater = new animationSprite({image:waterAnimations,rows:5,columns:8,position:{
+    x: 400,
+    y: 400
+},
+sizeOffset:10
+})
+const smithImage = new Image()
+smithImage.src = './assets/smith.png'
+const smith = new animationSprite({image:smithImage,rows:1, columns:8,position:{
+    x:835,
+    y:100
+},
+sizeOffset:32
+})
+
+const altarImage = new Image()
+altarImage.src = './assets/altar.png'
+const altar = new animationSprite({image:altarImage, rows:1, columns:39, position: {
+    x:285,
+    y:-830,
+},
+sizeOffset:200})
 
 const background = new Sprite({
     position: {
-        x: offset.x,
-        y:offset.y,
+        x: offset.x,//-463
+        y:offset.y,//-950
+        // x:-400,
+        // y:-150
     },
     image: image
 })
@@ -139,7 +215,7 @@ const testBoundary = new Boundary({
     }
 })
 
-const movables = [background, ...boundaries];
+const movables = [background, ...boundaries,testWater,smith,altar];
 function checkCollision({object1, object2}){
     return (object1.position.x + 192/4 -20 >= object2.position.x 
     && object1.position.x <= object2.position.x + object2.width
@@ -154,7 +230,11 @@ function animate(){
         boundary.draw()
         
     })
+    altar.draw()
     player.draw()
+    testWater.draw()
+    smith.draw()
+    
     moving = true
     player.moving = false
        if(keys.w.pressed && lastKey === 'w'){
@@ -172,7 +252,7 @@ function animate(){
             }
         }
         if(moving)
-        movables.forEach(movable => movable.position.y +=2)
+        movables.forEach(movable => movable.position.y +=20)
        }
        else if(keys.s.pressed && lastKey === 's'){
         player.moving = true
@@ -189,7 +269,7 @@ function animate(){
             }
         }
         if(moving)
-        movables.forEach(movable => movable.position.y -=2)
+        movables.forEach(movable => movable.position.y -=20)
        }
        else if(keys.a.pressed && lastKey === 'a'){
         player.moving = true
