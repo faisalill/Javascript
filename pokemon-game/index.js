67ff6e16f291fa4baa66d4d5e88e32026d57ccec
faisalill <mfaisal.ilyas.d@gmail.com>
2023-01-09 19:29:42 +0530

    player animation started

--- pokemon-game/index.js
+++ pokemon-game/index.js
@@ -50,11 +50,15 @@ class Sprite {
         this.image = image
         this.frames = {...frames, val:0, elapsed: 0}
         this.sizeOffset = sizeOffset
+        this.image.onload=()=>{
+            this.width = this.image.width/ this.frames.max
+            this.height = this.image.height
+        }
         this.moving = false
     }
     draw(){
         c.drawImage(this.image,
-            this.frames.val * this.image.width,
+            this.frames.val * this.width,
             0,
             this.image.width/this.frames.max, 
             this.image.height,
@@ -62,17 +66,16 @@ class Sprite {
             this.position.y,
             this.image.width/this.frames.max - this.sizeOffset ,
             this.image.height - this.sizeOffset)
+            if(this.moving){
+            if(this.frames.max >1){
+                this.frames.elapsed++
+            }
+            if(this.frames.elapsed % 10 ===0){
+                if(this.frames.val < this.frames.max -1) this.frames.val++
+                else this.frames.val = 0
+            }
+        }
             
-                if(this.frames.max > 1){
-                    this.frames.elapsed++
-                    if(this.frames.elapsed %10 ===0){
-                        if(this.frames.val < this.frames.max-1) this.frames.val++
-                        else{
-                            this.frames.val = 0
-                        }
-                    }
-                }
-                
             
     }
 }
