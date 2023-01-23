9a97dcfff7303742e5c65f186b48cac77f114457
faisalill <mfaisal.ilyas.d@gmail.com>
2023-01-23 17:39:43 +0530

    started minecraft clone using r3f

---
+++ react-three-fiber/minecraft-clone/src/images/images.js
@@ -0,0 +1,12 @@
+import oakPlank from './oakPlank.jpg'
+import glass from './glass.jpg'
+import dirt from './dirt.jpg'
+import stone from './stone.jpg'
+
+const images = {
+    oakPlank,
+    glass,
+    dirt,
+    stone
+}
+export default images
