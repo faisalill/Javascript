3e622549281b4a4af4cc17a18a3a8012bf6f86c6
faisalill <mfaisal.ilyas.d@gmail.com>
2023-01-25 22:09:02 +0530

    until helpers

--- r3f/components/AnimtedBox.jsx
+++ r3f/components/AnimtedBox.jsx
@@ -1,14 +1,18 @@
+import { useHelper } from "@react-three/drei";
 import { useFrame } from "@react-three/fiber";
 import { useRef } from "react";
+import { BoxHelper } from "three";
 
 
-export  const AnimatedBox = () =>{
+export  const AnimatedBox = ({isTesting}) =>{
    const meshRef = useRef(null)
    useFrame(()=>{
     if(meshRef.current){
-      meshRef.current.rotation.x += 0.01
+      meshRef.current.rotation.y += 0.009
+      meshRef.current.rotation.x -= 0.009
     }
    });
+   {isTesting? useHelper(meshRef,BoxHelper,'blue'):null}
 
   return (<mesh ref={meshRef}>
   <boxGeometry args={[1,1,1]} />
--- r3f/pages/index.js
+++ r3f/pages/index.js
@@ -1,14 +1,23 @@
-import { OrbitControls } from "@react-three/drei";
+import { OrbitControls, Stats } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import { AnimatedBox } from "@/components/AnimtedBox";
+
+
+
 export default function Home() {
+  const testing = true;
   return (
     <>
     <div className="canvas-container">
       <Canvas>
-        <ambientLight intensity={1}/>
+      
+        {testing? <axesHelper args={[5]}/> : null}
+        {testing? <Stats /> : null}
+        {testing? <gridHelper args={[10,10]}/> : null}
+        <directionalLight />
+        <ambientLight intensity={1} />
         <OrbitControls />
-        <AnimatedBox />
+        <AnimatedBox isTesting={testing}/>
       </Canvas>
       </div>
     </>
