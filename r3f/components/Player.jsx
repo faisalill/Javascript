69451743682b21cd1e7bb0f6269083fa4a71edef
faisalill <mfaisal.ilyas.d@gmail.com>
2023-01-27 23:12:15 +0530

    added keyboard input

--- r3f/components/Player.jsx
+++ r3f/components/Player.jsx
@@ -1,20 +1,31 @@
+import { useInput } from "./useInput"
+
 const { useGLTF, useAnimations } = require("@react-three/drei")
 const { useEffect } = require("react")
 
 const Player = ()=>{
   const model = useGLTF('./models/myPlayer.glb')
   const {actions} = useAnimations(model.animations,model.scene)
+   const {forward,backward,left,right,shift,jump} = useInput()
+  model.scene.traverse((object)=>{
+    if(object.isMesh){
+      object.castShadow = true
+    }
+  })
 
   useEffect(()=>{
-    // actions?.idle?.play()
-    console.log(model)
-    actions?.idle?.play()
-  },[])
+    console.log("forward",forward)
+    console.log("backward",backward)
+    console.log("left",left)
+    console.log("right",right)
+    console.log("shift",shift)
+    console.log("jump",jump)
+  },[forward,backward,left,right,shift,jump])
 
   return(
-    
+      <object3D>
       <primitive object={model.scene}/>
-    
+      </object3D>
   )
 }
 export default Player;
