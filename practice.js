102390f1fafb8f07803e9560182e81362d8b2ac7
faisalill <mfaisal.ilyas.d@gmail.com>
2023-04-25 23:48:42 +0530

    dsa

--- practice.js
+++ practice.js
@@ -318,6 +318,16 @@ var isAnagram = function(s, t) {
     return t === ""
 };
 //end
-
+//start 
+//Length of Last Word
+//https://leetcode.com/problems/length-of-last-word/
+// Given a string s consists of some words separated by spaces, return the length of the last word in the string. If the last word does not exist, return 0.
+// A word is a maximal substring consisting of non-space characters only.
+//Solution:
+var lengthOfLastWord = function(s) {
+    const t = s.trim()
+    return  t.split(" ")[t.split(" ").length-1].length
+};
+//end
 
 
