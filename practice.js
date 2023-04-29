a7ef470543d15c1695bf4ac3be4ce3a79ea68ff8
faisalill <mfaisal.ilyas.d@gmail.com>
2023-04-29 16:51:40 +0530

    dsa

--- practice.js
+++ practice.js
@@ -392,5 +392,26 @@ function search(nums, target) {
     return -1;
   }
 //end
+//start 
+//Min Cost Climbing Stairs
+//https://leetcode.com/problems/min-cost-climbing-stairs/
+// You are given an integer array cost where cost[i] is the cost of ith step on a staircase. Once you pay the cost, you can either climb one or two steps.
+// You can either start from the step with index 0, or the step with index 1.
+// Return the minimum cost to reach the top of the floor.
+//Solution:
+let even = cost[0];
+let odd = cost[1];
+
+for (let i = 2; i < cost.length; i++) {
+    if (i%2) {
+        odd = Math.min(odd + cost[i], even + cost[i]);
+    } else {
+        even = Math.min(even + cost[i], odd + cost[i]);
+    }
+}
+
+return Math.min(odd, even);
+//end
+
 
 
