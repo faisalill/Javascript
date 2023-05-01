c66974aec89384fd230373e736f5669ee322c886
faisalill <mfaisal.ilyas.d@gmail.com>
2023-05-01 22:30:49 +0530

    dsa

--- practice.js
+++ practice.js
@@ -412,6 +412,13 @@ for (let i = 2; i < cost.length; i++) {
 
 return Math.min(odd, even);
 //end
-
+//Find the Index of the First Occurrence in a String
+//https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string/description/
+//Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+//Solution:
+var strStr = function(haystack, needle) {
+    return haystack.indexOf(needle)
+};
+//end
 
 
